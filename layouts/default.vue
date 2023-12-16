0ae9bdceef6e6b68200185f3c52678b77a6c7368
cheetosysst <thect@protonmail.com>
2023-12-17 01:40:37 +0800

    fix: remove copied stuff

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,14 +14,9 @@ import { Menu } from "lucide-vue-next"
 						<Menu />
 					</label>
 				</div>
-				<div class="flex-1 items-baseline">
-					<a href="/" class="inline-grid">
-
-						<span
-							class="btn btn-ghost pointer-events-none col-start-1 row-start-1 text-3xl bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text blur-xl [transform:translate3d(0,0,0)] [-webkit-text-fill-color:transparent] before:content-[attr(data-text)] [@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]"
-							data-text="PAINTER" aria-hidden="true"></span>
-						<span
-							class="btn btn-ghost text-3xl [&amp;::selection]:text-base-content relative col-start-1 row-start-1 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text [-webkit-text-fill-color:transparent] [&amp;::selection]:bg-blue-700/20 [@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]">PAINTER</span>
+				<div class="flex-1 flex gap-2 items-baseline">
+					<a href="/" class="text-3xl btn btn-ghost">
+						PAINTER
 					</a>
 					<span>Placeholder for funny sentences that makes no sense</span>
 				</div>
